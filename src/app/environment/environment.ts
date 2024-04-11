374afd4bbb850b28819bdee979f56a80c07f5eb9
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-11 14:34:05 +0300

    Added class environment

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiIdentityAddress: 'http://localhost:5244/api',
+  ngrok: 'https://bf25-178-212-241-227.ngrok-free.app/api',
+  apiAddress: 'http://localhost:5052/api'
+}
