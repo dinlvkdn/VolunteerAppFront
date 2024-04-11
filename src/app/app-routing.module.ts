48282d44ac0c9dee04822e7e4aec5378b7327fe8
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-11 14:26:01 +0300

    Added routing to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {JobOffersPageModule} from "./pages/job-offers-page/job-offers-page.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : 'signup',
+    pathMatch : "full"
+  },
+  {
+    path : 'signup',
+    loadChildren: () => import('./pages/signup-page/signup-page.module')
+      .then(m => m.SignupPageModule)
+  },
+  {
+    path : 'signin',
+    loadChildren: () => import('./pages/signin-page/signin-page.module')
+      .then(m => m.SigninPageModule)
+  },
+  {
+    path : 'add-info',
+    loadChildren: () => import('./pages/add-info-page/add-info-page.module')
+      .then(m => m.AddInfoPageModule)
+  },
+  {
+    path : 'joboffers',
+    loadChildren: () => import('./pages/job-offers-page/job-offers-page.module')
+      .then(m => m.JobOffersPageModule)
+  },
+  {
+    path : 'job-offer',
+    loadChildren: () => import('./pages/job-offer-page/job-offer-page.module')
+      .then(m => m.JobOfferPageModule)
+  },
+  {
+    path : 'add-job-offer',
+    loadChildren: () => import('./pages/addnewjoboffer/add-job-offer-page.module')
+      .then(m => m.AddJobOfferPageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
