7365423041b8c4af4d5872e32d800e6e09f52479
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-19 15:54:28 +0300

    added the volunteer's confirmation dialog for the offer

---
+++ src/app/Shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.scss'
+})
+export class ConfirmationDialogComponent {
+constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>) {}
+  confirmVolunteerOnJobOffer(result: number) {
+    const data = {
+      action: result
+    };
+    this.dialogRef.close(data);
+  }
+}
